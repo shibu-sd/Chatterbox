2399c9eabdae14e0efda4697c0faeed45aa16b78
shibu-sd <sd.shibu007@gmail.com>
2023-08-31 00:04:22 +0530

    added animations to components

--- frontend/src/Components/CreateGroups.js
+++ frontend/src/Components/CreateGroups.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import DoneOutlineRoundedIcon from '@mui/icons-material/DoneOutlineRounded';
 import { IconButton } from '@mui/material';
+import { AnimatePresence, motion } from 'framer-motion';
 
 function CreateGroups() {
     return (
-        <div className='createGroups-container'>
-        <input placeholder='Enter Group Name' className='search-box' />
-        <IconButton>
-            <DoneOutlineRoundedIcon />
-        </IconButton>
-        </div>
+        <AnimatePresence>
+            <motion.div initial={{opacity:0, scale:0}} animate={{opacity:1, scale:1}} exit={{opacity:0, scale:0}} transition={{ease:"anticipate", duration:"0.3"}} 
+            className='createGroups-container'>
+                <input placeholder='Enter Group Name' className='search-box' />
+                <IconButton>
+                    <DoneOutlineRoundedIcon />
+                </IconButton>
+            </motion.div>
+        </AnimatePresence>
     )
 }
 
--- frontend/src/Components/Users.js
+++ frontend/src/Components/Users.js
@@ -3,39 +3,43 @@ import "./styles.css";
 import logo from "../Images/chatterbox-notext.png"
 import { IconButton } from '@mui/material';
 import PersonSearchIcon from '@mui/icons-material/PersonSearch';
+import { AnimatePresence, motion } from "framer-motion";
 
 function Users() {
     return (
-        <div className='list-container'>
-            <div className='ug-header'>
-                <img src={logo} alt='Logo' style={{ height: "2rem", width: "2rem", marginLeft: "10px" }} />
-                <p className='ug-title'>Online Users</p>
-            </div>
-            <div className='sidebar-search'>
-                <IconButton>
-                    <PersonSearchIcon />
-                </IconButton>
-                <input placeholder='Search' className='search-box' />
-            </div>
-            <div className='ug-list'>
-                <div className='list-item'>
-                    <p className='conversation-icon'>T</p>
-                    <p className='conversation-title'>Test User</p>
+        <AnimatePresence>
+            <motion.div initial={{opacity:0, scale:0}} animate={{opacity:1, scale:1}} exit={{opacity:0, scale:0}} transition={{ease:"anticipate", duration:"0.3"}}
+             className='list-container'>
+                <div className='ug-header'>
+                    <img src={logo} alt='Logo' style={{ height: "2rem", width: "2rem", marginLeft: "10px" }} />
+                    <p className='ug-title'>Online Users</p>
                 </div>
-                <div className='list-item'>
-                    <p className='conversation-icon'>T</p>
-                    <p className='conversation-title'>Test User</p>
+                <div className='sidebar-search'>
+                    <IconButton>
+                        <PersonSearchIcon />
+                    </IconButton>
+                    <input placeholder='Search' className='search-box' />
                 </div>
-                <div className='list-item'>
-                    <p className='conversation-icon'>T</p>
-                    <p className='conversation-title'>Test User</p>
+                <div className='ug-list'>
+                    <div className='list-item'>
+                        <p className='conversation-icon'>T</p>
+                        <p className='conversation-title'>Test User</p>
+                    </div>
+                    <div className='list-item'>
+                        <p className='conversation-icon'>T</p>
+                        <p className='conversation-title'>Test User</p>
+                    </div>
+                    <div className='list-item'>
+                        <p className='conversation-icon'>T</p>
+                        <p className='conversation-title'>Test User</p>
+                    </div>
+                    <div className='list-item'>
+                        <p className='conversation-icon'>T</p>
+                        <p className='conversation-title'>Test User</p>
+                    </div>
                 </div>
-                <div className='list-item'>
-                    <p className='conversation-icon'>T</p>
-                    <p className='conversation-title'>Test User</p>
-                </div>
-            </div>
-        </div >
+            </motion.div >
+        </AnimatePresence>
     )
 }
 
