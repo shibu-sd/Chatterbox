46a4244d7eff9c581eda74a34b032f1c6a09832d
shibu-sd <sd.shibu007@gmail.com>
2023-08-31 17:04:35 +0530

    created connection to database

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 const app = express();
 dotenv.config();
+
+const connectDB = async () => {
+    try {
+        const connect = await mongoose.connect(process.env.MONGO_URI);
+        console.log("Connected to MongoDB");
+    }
+    catch (err) {
+        console.log(err);
+    }
+};
+
+connectDB();
 
 app.get("/", (req, res) => {
     res.send("Hi");
 });
 
 const PORT = process.env.PORT;
+
+
+
 app.listen(PORT, console.log("Server is Running"));
